0ea06c07b046d7fe587a7326bf843fee6ce2b89d
adaptedbee <adaptedbee@yandex.ru>
2019-08-09 01:51:42 +0300

    От HTML к React 3. Данные из products.json

--- src/index.js
+++ src/index.js
@@ -2,15 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import './index.css';
+import products from './products.json';
 
 function App() {
+  const productsLimit = 3;
+
   return (
     <React.Fragment>
       <h1>Список товаров</h1>
       <ul>
-        <li>Имя товара 1</li>
-        <li>Имя товара 2</li>
-        <li>Имя товара 3</li>
+      {products.map((item, index) => index < productsLimit ? <li key={item.id}>{item.name}</li> : '')}
       </ul>
     </React.Fragment>
   );
