280bbc86e086a93128140f16c80e9ea09a5ac405
Alexander <vinokurov.ag@mail.ru>
2019-09-23 21:56:43 +0300

    Выполнил задание "От HTML к React"

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import products from "./products.json";
+
+import "./index.css"
+
+const ITEMS_NUMBER = 3;
+const rootElement = document.getElementById('root');
+
+function NumberList(props) {
+  const itemsNumber = props.itemsNumber;
+  const listItems = products.slice(0, itemsNumber).map((item, number) =>
+    <li key={number}>{item.name}</li>
+  );
+  return (
+    <ul>{listItems}</ul>
+  )
+}
+
+ReactDOM.render(
+  <div className = 'productsList'>
+    <h1>Список товаров</h1>
+    <NumberList itemsNumber={ITEMS_NUMBER} />,
+  </div>,
+  rootElement);
