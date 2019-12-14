14c28f7a534d1ad2d9dcaf461d3b06911505f1ec
Максим Водянов <max.post@bk.ru>
2019-12-14 21:27:16 +0300

    Redux Modularization

--- src/utils/getData.js
+++ src/utils/getData.js
@@ -1,39 +1,39 @@
-import memoize from './memoize';
 import {splitEvery} from 'csssr-school-utils'
+import {createSelector} from 'reselect';
 
-export const getFilteredData = memoize((obj) => {
-    const {
-        data,
-        minPrice,
-        maxPrice,
-        discount,
-        selectedCategories
-    } = obj;
-    return data.filter((item) => {
-        return item.price >= minPrice
-            && item.price <= maxPrice
-            && item.discount >= discount
-            && (selectedCategories.length > 0 ? selectedCategories.includes(item.category) : true)
-    });
-});
+export const getFilteredData = createSelector(
+    ({data}) => data,
+    ({minPrice}) => minPrice,
+    ({maxPrice}) => maxPrice,
+    ({discount}) => discount,
+    ({selectedCategories}) => selectedCategories,
+    (data, minPrice, maxPrice, discount, selectedCategories) => data.filter((item) => {
+            return item.price >= minPrice
+                && item.price <= maxPrice
+                && item.discount >= discount
+                && (selectedCategories.length > 0 ? selectedCategories.includes(item.category) : true)
+        }
+    )
+);
 
-export const getPaginationData = memoize((obj) => {
-    const {
-        data,
-        minPrice,
-        maxPrice,
-        discount,
-        selectedCategories,
-        itemsPerPage,
-        paginationActivePage
-    } = obj;
-    const filteredData = getFilteredData({
-        data,
-        minPrice,
-        maxPrice,
-        discount,
-        selectedCategories
-    });
-    return splitEvery(itemsPerPage, filteredData)[paginationActivePage - 1];
-});
+
+export const getPaginationData = createSelector(
+    ({data}) => data,
+    ({minPrice}) => minPrice,
+    ({maxPrice}) => maxPrice,
+    ({discount}) => discount,
+    ({selectedCategories}) => selectedCategories,
+    ({itemsPerPage}) => itemsPerPage,
+    ({paginationActivePage}) => paginationActivePage,
+    (data, minPrice, maxPrice, discount, selectedCategories, itemsPerPage, paginationActivePage) => {
+        const filteredData = getFilteredData({
+            data,
+            minPrice,
+            maxPrice,
+            discount,
+            selectedCategories
+        });
+        return splitEvery(itemsPerPage, filteredData)[paginationActivePage - 1];
+    }
+);
 
