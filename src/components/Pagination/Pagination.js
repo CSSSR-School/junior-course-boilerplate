e5955789cc036ce141bb84b29ed7d631da021842
Максим Водянов <max.post@bk.ru>
2019-12-14 13:50:39 +0300

    Redux Modularization

--- src/components/Pagination/Pagination.js
+++ src/components/Pagination/Pagination.js
@@ -7,7 +7,7 @@ const Pagination = props => {
 
     const {data, itemsPerPage, paginationActivePage, changePaginationActive} = props;
 
-    const handleClick = (value) => {
+    const handleChange = (value) => {
 
         const searchParams = new URLSearchParams(window.location.search);
         searchParams.set('page', value);
@@ -21,6 +21,8 @@ const Pagination = props => {
 
     const paginationLength = Math.ceil(data.length / itemsPerPage);
 
+    if (paginationActivePage > paginationLength) handleChange(1);
+
     if (data.length > 0) {
         return (
             <ul className={s.Pagination}>
@@ -28,14 +30,14 @@ const Pagination = props => {
                     <button type="button"
                             disabled={+paginationActivePage === 1}
                             className={s.PaginationButton}
-                            onClick={() => handleClick(+paginationActivePage - 1)}>
+                            onClick={() => handleChange(+paginationActivePage - 1)}>
                         Назад
                     </button>
                 </li>
 
                 {[...Array(paginationLength)].map((item, key) => (
                     <li className={cx(s.PaginationItem, {[s.PaginationItemActive]: key + 1 === +paginationActivePage})} key={key}>
-                        <button type="button" className={s.PaginationButton} onClick={() => handleClick(key + 1)}>{key + 1}</button>
+                        <button type="button" className={s.PaginationButton} onClick={() => handleChange(key + 1)}>{key + 1}</button>
                     </li>
                 ))}
 
@@ -44,7 +46,7 @@ const Pagination = props => {
                     <button type="button"
                             disabled={+paginationActivePage === paginationLength}
                             className={s.PaginationButton}
-                            onClick={() => handleClick(+paginationActivePage + 1)}>
+                            onClick={() => handleChange(+paginationActivePage + 1)}>
                         Вперед
                     </button>
                 </li>
