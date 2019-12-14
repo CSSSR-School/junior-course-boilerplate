e52405ea21055c6363e2414576d120e23147c599
Максим Водянов <max.post@bk.ru>
2019-12-14 22:01:43 +0300

    Redux Modularization

--- src/containers/ListContainer.js
+++ src/containers/ListContainer.js
@@ -6,13 +6,9 @@ import {getPaginationData} from '../utils/getData';
 const mapStateToProps = ({filter, pagination}) => ({
 
     data: getPaginationData({
-        data: data,
-        minPrice: filter.minPrice,
-        maxPrice: filter.maxPrice,
-        discount: filter.discount,
-        selectedCategories: filter.selectedCategories,
-        itemsPerPage: pagination.itemsPerPage,
-        paginationActivePage: pagination.paginationActivePage
+        data,
+        ...filter,
+        ...pagination
     })
 });
 
--- src/containers/PaginationContainer.js
+++ src/containers/PaginationContainer.js
@@ -5,15 +5,12 @@ import {getFilteredData} from '../utils/getData';
 import data from '../products';
 
 const mapStateToProps = ({pagination, filter}) => ({
-    paginationActivePage: pagination.paginationActivePage,
-    itemsPerPage: pagination.itemsPerPage,
-
+    ...filter,
+    ...pagination,
     data: getFilteredData({
         data: data,
-        minPrice: filter.minPrice,
-        maxPrice: filter.maxPrice,
-        discount: filter.discount,
-        selectedCategories: filter.selectedCategories
+        ...filter,
+        ...pagination
     })
 });
 
