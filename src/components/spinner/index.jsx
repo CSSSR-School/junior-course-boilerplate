1a3bb275a4d2003cb951f2a6d7d5faec1fd66691
ensisf <zamchenco@gmail.com>
2019-12-15 18:07:20 +0200

    Add spinner

---
+++ src/components/spinner/index.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import styles from "./styles.module.scss";
+
+const Spinner = ({ size = "1em", className = "", ...attrs }) => {
+  return (
+    <svg
+      width={size}
+      height={size}
+      className={`${styles.spinner} ${className}`}
+      viewBox="0 0 100 100"
+      {...attrs}
+    >
+      <circle
+        cx="50"
+        cy="50"
+        fill="none"
+        strokeLinecap="round"
+        r="40"
+        strokeWidth="4"
+        stroke="currentColor"
+        strokeDasharray="62.83185307179586 62.83185307179586"
+        transform="rotate(264.014 50 50)"
+      >
+        <animateTransform
+          attributeName="transform"
+          type="rotate"
+          calcMode="linear"
+          values="0 50 50;360 50 50"
+          keyTimes="0;1"
+          dur="1s"
+          begin="0s"
+          repeatCount="indefinite"
+        ></animateTransform>
+      </circle>
+    </svg>
+  );
+};
+
+export { Spinner };
