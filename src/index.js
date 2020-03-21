b665316a3ee49fc2dd635e9511309dfc675bc374
Andrey Karamnoff <karamnoff@gmail.com>
2020-03-21 09:54:38 +0300

    add 'Open Sans' from webfontloader

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app';
+import WebFont from 'webfontloader';
+
+WebFont.load({
+  google: {
+    families: ['Open Sans:300,600', 'sans-serif']
+  }
+});
 
 ReactDOM.render(<App />,
   document.getElementById('root'));
