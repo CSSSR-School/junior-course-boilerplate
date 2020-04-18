f6be6ba7f2dd4fb8399a12196198a67446f51c4c
Andrey Karamnoff <karamnoff@gmail.com>
2020-04-18 10:48:58 +0300

    make refactoring for Pagination

--- src/components/pagination/pagination.js
+++ src/components/pagination/pagination.js
@@ -6,133 +6,69 @@ import styles from './pagination.module.scss';
 import Btn from './btn';
 
 class Pagination extends React.Component {
-  getPagesTotalCount = (length, n) => Math.ceil(length / n);
-
-  handleClick = event => {
-    const { updatePaginationCurrentPage } = this.props;
-    event.preventDefault();
-    const {
-      target: { textContent }
-    } = event;
-    const value = Number(textContent);
-
-    updatePaginationCurrentPage({ currentPage: value });
-  };
-
-  handleIncClick = () => {
-    const {
-      pagination: { upperPageBound },
-      updatePaginationCurrentPage,
-      shiftPaginationPageBoundsForward
-    } = this.props;
-    shiftPaginationPageBoundsForward();
-    updatePaginationCurrentPage({ currentPage: upperPageBound + 1 });
-  };
-
-  handleDecClick = () => {
-    const {
-      pagination: { upperPageBound, pageBound },
-      updatePaginationCurrentPage,
-      shiftPaginationPageBoundsBack
-    } = this.props;
-    shiftPaginationPageBoundsBack();
-    updatePaginationCurrentPage({ currentPage: upperPageBound - pageBound });
-  };
-
-  handlePrevClick = () => {
-    const {
-      pagination: { currentPage, pageBound },
-      shiftPaginationPageBoundsBack,
-      updatePaginationCurrentPage
-    } = this.props;
-    const prevPage = currentPage - 1;
-    if (prevPage % pageBound === 0) {
-      shiftPaginationPageBoundsBack();
-    }
-
-    updatePaginationCurrentPage({ currentPage: prevPage });
-  };
-
-  handleNextClick = () => {
-    const {
-      pagination: { currentPage, upperPageBound },
-      shiftPaginationPageBoundsForward,
-      updatePaginationCurrentPage
-    } = this.props;
-    const nextPage = currentPage + 1;
-
-    if (nextPage > upperPageBound) {
-      shiftPaginationPageBoundsForward();
-    }
-    updatePaginationCurrentPage({ currentPage: nextPage });
-  };
 
   render() {
     const {
-      pagination: { currentPage, itemsPerPage, upperPageBound, lowerPageBound },
-      list: productsList
+      pagination: { currentPage, upperPageBound, lowerPageBound },
+      pagesLength,
     } = this.props;
-    const pagesTotalCount = Array.from(
-      { length: this.getPagesTotalCount(productsList.length, itemsPerPage) },
+    const pagesData = Array.from(
+      { length: pagesLength },
       (value, index) => index + 1
     );
-    const pages = pagesTotalCount.map(number => {
+    const pages = pagesData.map(number => {
       if (number < upperPageBound + 1 && number > lowerPageBound) {
         return (
           <Btn
             key={number}
-            classList={classnames(
-              styles.PaginationBtn,
-              styles.PaginationBtnPage,
-              {
-                [styles.PaginationBtnActive]: number === currentPage
-              }
-            )}
-            onClick={this.handleClick}
+            classList={classnames(styles.PaginationBtn, {
+              [styles.PaginationBtnActive]: number === currentPage
+            })}
             value={String(number)}
+            handleClick={event => this.props.handleClick(event, 'current')}
           />
         );
       }
       return null;
     });
     const inc =
-      pagesTotalCount.length > upperPageBound ? (
+    pagesData.length > upperPageBound ? (
         <Btn
-          classList={classnames(styles.PaginationBtn, styles.PaginationBtnInc)}
-          handleClick={this.handleIncClick}
+          classList={classnames(styles.PaginationBtn)}
           value="&hellip;"
+          handleClick={event => this.props.handleClick(event, 'inc')}
         />
       ) : null;
     const dec =
       lowerPageBound >= 1 ? (
         <Btn
-          classList={classnames(styles.PaginationBtn, styles.PaginationBtnDec)}
-          handleClick={this.handleDecClick}
+          classList={classnames(styles.PaginationBtn)}
           value="&hellip;"
+          handleClick={event => this.props.handleClick(event, 'dec')}
         />
       ) : null;
     const prev =
-      pages.length !== 0 ? (
+    pagesData.length !== 0 ? (
         <Btn
           classList={classnames(styles.PaginationBtn, styles.PaginationBtnPrev)}
-          handleClick={this.handlePrevClick}
           isDisabled={
             currentPage === 1 ||
-            (pagesTotalCount !== currentPage && pagesTotalCount < 1)
+            (pagesData !== currentPage && pagesData < 1)
           }
           value="Назад"
+          handleClick={event => this.props.handleClick(event, 'prev')}
         />
       ) : null;
     const next =
-      pages.length !== 0 ? (
+    pagesData.length !== 0 ? (
         <Btn
           classList={classnames(styles.PaginationBtn, styles.PaginationBtnNext)}
-          handleClick={this.handleNextClick}
           isDisabled={
-            pagesTotalCount.length === currentPage ||
-            (currentPage !== 1 && pagesTotalCount < 1)
+            pagesData.length === currentPage ||
+            (currentPage !== 1 && pagesData < 1)
           }
           value="Вперед"
+          handleClick={event => this.props.handleClick(event, 'next')}
         />
       ) : null;
 
@@ -158,23 +94,8 @@ Pagination.propTypes = {
     lowerPageBound: propTypes.number,
     pageBound: propTypes.number
   }),
-  list: propTypes.arrayOf(
-    propTypes.shape({
-      id: propTypes.number,
-      isInStock: propTypes.bool,
-      img: propTypes.string,
-      title: propTypes.node,
-      price: propTypes.node,
-      subPriceContent: propTypes.node,
-      maxRating: propTypes.number,
-      rating: propTypes.number,
-      discount: propTypes.number,
-      category: propTypes.string
-    })
-  ),
-  updatePaginationCurrentPage: propTypes.func,
-  shiftPaginationPageBoundsBack: propTypes.func,
-  shiftPaginationPageBoundsForward: propTypes.func
+  pagesLength: propTypes.number,
+  handleClick: propTypes.func,
 };
 
 export default Pagination;
--- src/containers/pagination-container/pagination-container.js
+++ src/containers/pagination-container/pagination-container.js
@@ -10,21 +10,63 @@ import { paginationActions } from '../../redux';
 import Pagination from '../../components/pagination';
 
 class PaginationContainer extends PureComponent {
-  render() {
+  getPagesTotalCount = (length, n) => Math.ceil(length / n);
+
+  handleClick = (event, type) => {
+    event.preventDefault();
+
     const {
-      pagination,
-      list,
-      updatePaginationCurrentPage,
+      pagination: { currentPage, upperPageBound, pageBound },
       shiftPaginationPageBoundsBack,
-      shiftPaginationPageBoundsForward
+      shiftPaginationPageBoundsForward,
+      updatePaginationCurrentPage
     } = this.props;
+
+    switch (type) {
+      case 'prev':
+        const prevPage = currentPage - 1;
+        if (prevPage % pageBound === 0) {
+          shiftPaginationPageBoundsBack();
+        }
+        updatePaginationCurrentPage({ currentPage: prevPage });
+        break;
+      case 'dec':
+        shiftPaginationPageBoundsBack();
+        updatePaginationCurrentPage({
+          currentPage: upperPageBound - pageBound
+        });
+        break;
+      case 'current':
+        const {
+          target: { textContent }
+        } = event;
+        const value = Number(textContent);
+        updatePaginationCurrentPage({ currentPage: value });
+        break;
+      case 'inc':
+        shiftPaginationPageBoundsForward();
+        updatePaginationCurrentPage({ currentPage: upperPageBound + 1 });
+        break;
+      case 'next':
+        const nextPage = currentPage + 1;
+
+        if (nextPage > upperPageBound) {
+          shiftPaginationPageBoundsForward();
+        }
+        updatePaginationCurrentPage({ currentPage: nextPage });
+        break;
+      default:
+        throw new Error(`Unknown type: ${type}`);
+    }
+  };
+  render() {
+    const { pagination, list } = this.props;
+    const { itemsPerPage } = pagination;
     return (
       <Pagination
         pagination={pagination}
-        list={list}
-        updatePaginationCurrentPage={updatePaginationCurrentPage}
-        shiftPaginationPageBoundsBack={shiftPaginationPageBoundsBack}
-        shiftPaginationPageBoundsForward={shiftPaginationPageBoundsForward}
+        pagesLength={this.getPagesTotalCount(list.length, itemsPerPage)}
+        handleClick={this.handleClick}
       />
     );
   }
