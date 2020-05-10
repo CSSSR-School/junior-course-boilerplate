a3531740eae25ddbc2593942d66c69424c8bac09
Andrey Karamnoff <karamnoff@gmail.com>
2020-05-10 11:31:07 +0300

    make pagination refactoring

--- src/components/pagination/pagination.js
+++ src/components/pagination/pagination.js
@@ -10,39 +10,43 @@ class Pagination extends React.Component {
       pagesLength
     } = this.props;
 
-    const pagesData = Array.from(
-      { length: pagesLength },
-      (value, index) => index + 1
-    );
-
-    const pages = pagesData.map(number => {
-      return (
-        number < upperPageBound + 1 &&
-        number > lowerPageBound &&
-        renderButton(this.props, number)
+    if (pagesLength !== 0) {
+      const pagesData = Array.from(
+        { length: pagesLength },
+        (value, index) => index + 1
       );
-    });
 
-    const inc =
-      pagesData.length > upperPageBound && renderButton(this.props, 'inc');
+      const pages = pagesData.map(number => {
+        return (
+          number < upperPageBound + 1 &&
+          number > lowerPageBound &&
+          renderButton(this.props, number)
+        );
+      });
 
-    const dec = lowerPageBound >= 1 && renderButton(this.props, 'dec');
+      const inc =
+        pagesData.length > upperPageBound && renderButton(this.props, 'inc');
 
-    const prev = pagesLength !== 0 && renderButton(this.props, 'prev');
+      const dec = lowerPageBound >= 1 && renderButton(this.props, 'dec');
 
-    const next = pagesLength !== 0 && renderButton(this.props, 'next');
+      const prev = pagesLength !== 0 && renderButton(this.props, 'prev');
 
-    return (
-      <div className={styles.Pagination}>
-        <ul className={styles.PaginationList}>
-          {prev}
-          {dec}
-          {pages}
-          {inc}
-          {next}
-        </ul>
-      </div>
-    );
+      const next = pagesLength !== 0 && renderButton(this.props, 'next');
+
+      return (
+        <div className={styles.Pagination}>
+          <ul className={styles.PaginationList}>
+            {prev}
+            {dec}
+            {pages}
+            {inc}
+            {next}
+          </ul>
+        </div>
+      );
+    }
+
+    return null;
   }
 }
 
