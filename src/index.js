98386c8252488939742455759c0a6da7e22d9b6e
maXimalist <www.maximalist@mail.ru>
2021-02-24 17:19:02 +0300

    task-1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-alert('hello world. This is react!');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import products from './products.json';
+import './index.css';
+
+const PRODUCTS_LIMIT = 3;
+
+const App = () => {
+  const productsList = products.slice(0, PRODUCTS_LIMIT);
+
+  return (
+    <div className="container">
+      <h1>Список товаров</h1>
+      <ul>
+        {productsList.map(({id, name}) => <li key={id}>{name}</li>)}
+      </ul>
+    </div>
+  )
+}
+
+ReactDOM.render(<App/>, document.getElementById('root'));
