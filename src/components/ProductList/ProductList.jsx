0c677f1a04ff74f9fda5e503354d733cd21035e4
Dmitrii Kirshanov <ext.dkirshanov@tinkoff.ru>
2021-04-25 22:19:00 +0200

    Подключен пакет csssr-school-product-card, исправлена передача пропов в ProductList

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import ProductItem from '../ProductItem/ProductItem';
+import ProductItem from 'csssr-school-product-card';
 import Rating from '../Rating/Rating';
 
 import products from '../../products.json';
@@ -8,10 +8,25 @@ import './ProductList.css';
 
 const ProductList = () => (
   <ul className="product-list">
-    {products.map(({ id, ...props }) =>
+    {products.map(({
+      id,
+      img,
+      isInStock,
+      maxRating,
+      price,
+      rating,
+      subPriceContent,
+      title,
+    }) =>
       <ProductItem
         key={id}
-        {...props}
+        img={ img}
+        isInStock={isInStock}
+        maxRating={maxRating}
+        price={price}
+        rating={rating}
+        subPriceContent={subPriceContent}
+        title={title}
         ratingComponent={Rating}
       />
     )}
