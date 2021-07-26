abd3df1f8fb18fa5c675524c610eaf94c0dad79b
PavelProlubnikov <pavelprolubnikov@gmail.com>
2021-07-26 18:29:03 +0300

    -

--- src/components/ProductPage/ProductsFilter/ProductsFilter.js
+++ src/components/ProductPage/ProductsFilter/ProductsFilter.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import s from './ProductsFilter.module.css';
+import {toInt} from 'csssr-school-utils';
 
 class ProductsFilter extends React.Component {
     constructor(props) {
@@ -13,16 +14,13 @@ class ProductsFilter extends React.Component {
         this.handleSubmit = this.handleSubmit.bind(this);
         this.handleChangeMin = this.handleChangeMin.bind(this);
         this.handleChangeMax = this.handleChangeMax.bind(this);
-        // this.input = React.createRef();
     }
 
     getMinValue(arr) {
         // делаем глубокую копию
         const temp = JSON.parse(JSON.stringify(arr));
         // убираем нечисловые символы
-        temp.forEach((item) => {
-            item.price = +item.price.replace(/\D/g, '');
-        });
+        temp.forEach(toInt(temp.price));
         // сортируем по возрастанию
         temp.sort((a, b) => {
             return a.price > b.price ? 1 : -1;
@@ -30,36 +28,16 @@ class ProductsFilter extends React.Component {
         return temp[0].price;
     }
 
-    getMaxValue(arr) {
-        // делаем глубокую копию
-        const temp = JSON.parse(JSON.stringify(arr));
-        // убираем нечисловые символы
-        temp.forEach((item) => {
-            item.price = +item.price.replace(/\D/g, '');
-        });
-        // сортируем по возрастанию
-        temp.sort((a, b) => {
-            return a.price > b.price ? 1 : -1;
-        });
-        return temp.reverse()[0].price;
-    }
-
     handleChangeMin(event) {
         if (Math.sign(event.target.value) === 0 || Math.sign(event.target.value) === 1) {
             this.setState({minValue: event.target.value})
-        } else {
-            alert('Введите ноль или положительное число');
         }
-        
     }
 
     handleChangeMax(event) {
         if (Math.sign(event.target.value) === 0 || Math.sign(event.target.value) === 1) {
             this.setState({maxValue: event.target.value})
-        } else {
-            alert('Введите положительное число');
         }
-        this.setState({maxValue: event.target.value})
     }
 
     handleSubmit(event) {
