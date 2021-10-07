265391e8b646395c0e8a121475c7ee8ab54da7c9
PavelProlubnikov <pavelprolubnikov@gmail.com>
2021-10-07 19:17:51 +0300

    в процессе реализации мемоизации

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ProductPage from './components/ProductPage/ProductPage.js';
 import data from './products.json';
 import { maxBy, minBy, toInt } from 'csssr-school-utils';
+import * as R from 'ramda';
 
 function getInt(arr) {
     arr.forEach((item) => {
@@ -19,14 +20,25 @@ function getMaxValue(arr) {
 }
 
 function getFilteredProducts(arr, min, max, sale) {
-    const filtered = arr.filter((item) => {
+    return arr.filter((item) => {
         if (item.discount >= sale) {
             return (item.price >= min) && (item.price <= max);
         }
     });
-
-    return filtered;
 }
+
+function memoizeByResult(fn) {
+    let memoizedResult = null;
+    
+    fn((result) => {
+        if (!R.equals(memoizedResult, result)) {
+            memoizedResult = result;
+        }
+    });
+    return memoizedResult;
+}
+
+let memoizedGetFilteredProducts = memoizeByResult(getFilteredProducts);
 
 class App extends React.PureComponent {
     constructor(props) {
@@ -40,6 +52,7 @@ class App extends React.PureComponent {
     }
 
     handleChange(event) {
+        console.log('App#hangleChange');
         if (event.target.value === '') {
             this.setState({
                 [event.target.name]: event.target.value
@@ -54,11 +67,12 @@ class App extends React.PureComponent {
     render() {
         console.log('App#render');
         const {minValue, maxValue, sale} = this.state;
-        const filteredProducts = getFilteredProducts(data, minValue, maxValue, sale);
+        const filteredProducts = memoizedGetFilteredProducts(data, minValue, maxValue, sale);
         // логи для проверки
-        console.log('minValue:', minValue);
-        console.log('maxValue:', maxValue);
-        console.log('sale', sale);
+        // console.log(filteredProducts);
+        // console.log('minValue:', minValue);
+        // console.log('maxValue:', maxValue);
+        // console.log('sale', sale);
 
         return <ProductPage
             filteredProducts={filteredProducts}
