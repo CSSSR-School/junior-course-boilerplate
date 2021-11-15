25dcb466e5066f44249e87b9a0590aaa857b0dd2
PavelProlubnikov <pavelprolubnikov@gmail.com>
2021-11-15 23:14:28 +0300

    в работе

--- src/App.js
+++ src/App.js
@@ -3,40 +3,6 @@ import ProductPage from './components/ProductPage/ProductPage.js';
 import data from './products.json';
 import { maxBy, minBy, toInt } from 'csssr-school-utils';
 // import * as R from 'ramda';
-  
-function getMinValue(arr) {
-    return toInt(minBy((obj) => toInt(obj.price), arr).price);
-}
-  
-function getMaxValue(arr) {
-    return toInt(maxBy((obj) => toInt(obj.price), arr).price);
-}
-
-function getFilteredProducts(arr, min, max, sale, selectedCategories) {
-    return arr.filter((item) => {
-        const priceItem = toInt(item.price);
-        if (item.discount >= sale) {
-            if (selectedCategories = []) {
-                return (priceItem >= min) && (priceItem <= max);
-            }
-            else {
-                for (let elem of selectedCategories) {
-                    return (priceItem >= min) && (priceItem <= max) && (item.category === elem);
-                }
-            }
-        }
-    });
-}
-
-function getSelectedCategories(selectedCategories, name) {
-    let index = selectedCategories.indexOf(name);
-    if (index > -1) {
-        selectedCategories.splice(index, 1);
-    } else {
-        selectedCategories.push(name);
-    }
-    return selectedCategories;
-}
 
 /* на будущее
 function memoizeByResult(fn) {
@@ -54,7 +20,32 @@ function memoizeByResult(fn) {
 let memoizedGetFilteredProducts = memoizeByResult(getFilteredProducts);
 */
 
-let selectedCategories = [];
+function getMinValue(arr) {
+    return toInt(minBy((obj) => toInt(obj.price), arr).price);
+}
+  
+function getMaxValue(arr) {
+    return toInt(maxBy((obj) => toInt(obj.price), arr).price);
+}
+
+function getFilteredProducts(arr, min, max, sale, selectedCategories) {
+    return arr.filter((item) => {
+        const priceItem = toInt(item.price);
+        if (item.discount >= sale) {
+            return (priceItem >= min) && (priceItem <= max) && selectedCategories.includes(item.category);
+        }
+    });
+}
+
+// function getSelectedCategories(selected, name) {
+//     let index = selected.indexOf(name);
+//     if (index > -1) {
+//         selected.splice(index, 1);
+//     } else {
+//         selected.push(name);
+//     }
+//     return selected;
+// }
 
 const CategoryContext = React.createContext({
     handleSelectCategory: () => {},
@@ -63,8 +54,10 @@ const CategoryContext = React.createContext({
 class App extends React.PureComponent {
     constructor(props) {
         super(props);
+        //перезаписываем первоначальный URL, чтобы при возвращении к самому первому реальному URL в нашей истории не было ошибки (когда свойство state события popstate будет равно null)
         const url = window.location.pathname.substr(1);
         window.history.replaceState({ url }, 'title', window.location.pathname);
+
         this.state = {
             minValue: getMinValue(data),
             maxValue: getMaxValue(data),
@@ -86,14 +79,23 @@ class App extends React.PureComponent {
     }
 
     setFromHistory = (event) => {
-        this.setState({ category: event.state['url'] });
+        this.setState({ selectedCategories: event.state['url'] });
     }
 
     handleSelectCategory(e) {
-        getSelectedCategories(selectedCategories, e.target.name);
+        const selectedItem = e.target.name;
+        const { selectedCategories } = this.state;
+        let selected = [];
+
+        if (selectedCategories.includes(selectedItem)) {
+             selected = selectedCategories.filter((item) => item !== selectedItem);
+        } else {
+            selected = [...selectedCategories, selectedItem];
+        }
+
         this.setState({
-            selectedCategories: selectedCategories
-        })
+            selectedCategories: selected
+        });
 
         const url = e.target.name;
         window.history.pushState({ url }, 'title', url);
@@ -115,6 +117,7 @@ class App extends React.PureComponent {
         this.setState({ minValue: getMinValue(data),
                         maxValue: getMaxValue(data),
                         sale: 0,
+                        selectedCategories: []
                     });
     }
 
@@ -122,7 +125,7 @@ class App extends React.PureComponent {
         const {minValue, maxValue, sale, selectedCategories} = this.state;
         const filteredProducts = getFilteredProducts(data, minValue, maxValue, sale, selectedCategories);
         console.log('filteredProducts', filteredProducts);
-        console.log('selectedCategories', selectedCategories);
+        console.log('selectedCategories', this.state.selectedCategories);
 
         return (
             <CategoryContext.Provider value={{
