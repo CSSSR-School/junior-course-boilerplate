8212f872906a69ee824929b961ed5e4e9068c270
kmpeeduwee <kartsevsb@gmail.com>
2022-11-28 22:52:24 +0300

    Add react application

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Plist from './Plist';
+
+function App() {
+    return(
+        <div className="App">
+            <header className="App-header">
+                <h1>Список товаров</h1>
+            </header>
+            <main className="App-main">
+                <Plist />
+            </main>
+        </div>
+    );
+}
+
+export default App;
